9271274941ffc1848309f1f4691794b2f751e60c
Gwonyeong <rnjsdud980@naver.com>
2023-01-09 21:17:16 +0900

    Add: create typescript example

---
+++ typesciprt/typescript.ts
@@ -0,0 +1,90 @@
+// 1. 타입 스크립트
+// [선언 키워드][변수명] : [타입]
+
+const numberA: number = 1;
+const nickname: string = "gwonyeong";
+
+// var와 let은 호이스팅의 차이
+
+// 2. 타입스크립트는 자바스크립트가 가지는 자료형을 모두 포함함.
+
+const var1: boolean = true; // 참, 거짓을 나타내는 논리값
+const var2: null = null; // 유효하지 않음을 나타냄.
+const var3: undefined = undefined; // 값이 존재하지 않음.
+const var4: number = 1; //-(2^53 -1)과 2^53 -1 사이의 정수, 실수
+const var5: bigint = 2n; // Number의 범위를 넘어서는 정수를 안전하게 저장하고 연산. 정수 끝에 n을 추가해야함.
+const var6: string = "gwonyeong"; // 문자열, 변경 불가능 함.
+
+// 객체 타입
+const people = {
+    name: "gwonyeong",
+    age: 25,
+    hobby: ["hearing music"]
+};
+
+// 인터페이스
+// '틀'을 정해놓는 것임.
+
+interface User {
+    name: string;
+    age: number;
+}
+
+const member1: User = {
+    name: "gwonyeong",
+    age: 25
+};
+
+// User라는 타입으로 지정해놓고 이 틀에 맞지 않는 타입을 넣으면 에러가 발생함.
+
+// 3. 유니온 타입
+
+function getLength(obj: string | string[]) {
+    return obj;
+}
+// 위의 타입은 stirng과 string[] 타입 중 하나를 가질 수 있음.
+
+// 또한 유니온 타입은 변수가 가질 수 없는 값을 제한할 수도 있음.
+
+type Status = "Ready" | "Waiting";
+
+const status1: Status = "Ready";
+// const errStatus2: Status = "ready"; // 에러가 발생
+
+// 열거형 enum
+enum StatusEnum {
+    READY = "Ready",
+    WAITING = "Waitinh"
+}
+
+// 4. 제네릭 타입
+// 어떠한 타입이든 정의될 수 있지만 호출되는 시점에 타입이 결정됨.
+
+function identity<T>(arg: T): T {
+    return arg;
+}
+// 함수는 기본적으로 함수의 타입과 같은 타입을 반환해주어야함.
+// 함수가 number값이라면 return으로 넘겨주는 값도 number타입이여야 하는 것.
+
+identity(10);
+
+//-----------------
+// 데코레이터
+
+// 횡단 관심사를 분리하여 관점 지향 프로그래밍을 적용한 코드를 작성할 수 있음.
+// 클래스, 메서드, 접근자, 프로퍼티, 매개변수에 적용 가능.
+// 각 요소에 @로 시작하는 데코레이터를 선언하면 데코레이터로 구현된 코드를 함께 실행함.
+
+function deco(target: any) {
+    console.log("deco");
+}
+
+class TestClass {
+    @deco
+    test() {
+        console.log("function");
+    }
+}
+
+const test = new TestClass();
+console.log(test.test());
